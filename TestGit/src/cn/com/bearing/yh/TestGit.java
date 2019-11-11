d3fd9573d9936479feef9beddb1b6252a2601f24
daiguanghao-hub <13733086100@163.com>
2019-11-11 17:20:08 +0800

    test

--- TestGit/src/cn/com/bearing/yh/TestGit.java
+++ TestGit/src/cn/com/bearing/yh/TestGit.java
@@ -3,5 +3,8 @@ package cn.com.bearing.yh;
 public class TestGit {
 	public void method(){
 		System.out.println("111");
+		System.out.println("999");
+		System.out.println("888");
+		System.out.println("000");
 	}
 }
